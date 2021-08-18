1d0012738ab8084c34d76cdf8a2db512842201c8
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-08-18 14:10:59 +0100

    updated settings

--- zuritask/settings.py
+++ zuritask/settings.py
@@ -61,10 +61,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'homepage.apps.HomepageConfig',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
